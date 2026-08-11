b9916ef4d85739bcaf98172d776cd9c9ab7467c9
Rahian S.S. <ter26621@gmail.com>
2026-08-11 13:20:05 -0300

    Finalizando o endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "Valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResults = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResults.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResults.rows[0].count;
+
+  res.status(200).json({
+    updated_At: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET api/v1/status should return status code 200", async () => {
   const result = await fetch("http://localhost:3000/api/v1/status");
   expect(result.status).toBe(200);
+
+  const responseBody = await result.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_At).toISOString();
+  expect(responseBody.updated_At).toEqual(parsedUpdatedAt);
+
+  const resDepDatabase = responseBody.dependencies.database;
+
+  expect(resDepDatabase.version).toBe("16.0");
+  expect(resDepDatabase.max_connections).toBe(100);
+  expect(resDepDatabase.opened_connections).toBe(1);
+
+  console.log(responseBody);
 });
