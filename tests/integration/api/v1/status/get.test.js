cab61e88fd311f90df1215a6ac1c7e78296f76c5
Rahian S.S. <85490097+TheRazorbill@users.noreply.github.com>
2026-08-31 22:33:37 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,18 +4,22 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET api/v1/status should return status code 200", async () => {
-  const result = await fetch("http://localhost:3000/api/v1/status");
-  expect(result.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const result = await fetch("http://localhost:3000/api/v1/status");
+      expect(result.status).toBe(200);
 
-  const responseBody = await result.json();
+      const responseBody = await result.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_At).toISOString();
-  expect(responseBody.updated_At).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_At).toISOString();
+      expect(responseBody.updated_At).toEqual(parsedUpdatedAt);
 
-  const resDepDatabase = responseBody.dependencies.database;
+      const resDepDatabase = responseBody.dependencies.database;
 
-  expect(resDepDatabase.version).toBe("16.0");
-  expect(resDepDatabase.max_connections).toBe(100);
-  expect(resDepDatabase.opened_connections).toBe(1);
+      expect(resDepDatabase.version).toBe("16.0");
+      expect(resDepDatabase.max_connections).toBe(100);
+      expect(resDepDatabase.opened_connections).toBe(1);
+    });
+  });
 });
